ed6fbedb98cde90ac18a503bdcea8dc308ed818f
Guillermo Indalecio Fernandez <guillermobox@gmail.com>
2020-06-21 17:21:53 +0200

    Add echo using a Line structure
    This just adds echo using a Line structure.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::io::BufRead;
+
+#[derive(Debug)]
+struct Line<'a> {
+    file: &'a String,
+    position: u32,
+    content: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let filepath = std::env::args().nth(1).unwrap(); // yes I know
+    let fhandler = std::fs::File::open(&filepath).unwrap();
+    let reader = std::io::BufReader::new(fhandler);
+
+    for content in reader.lines() {
+        let line = Line {
+            file: &filepath,
+            position: 7,
+            content: content.unwrap(),
+        };
+        println!("{}", line.content);
+    }
 }
