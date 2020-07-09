e42b73e41956eccdf5a4c47940a49d6e63826a18
Guillermo Indalecio Fernandez <guillermobox@gmail.com>
2020-07-09 20:02:02 +0200

    Remove builder pattern

--- src/parser.rs
+++ src/parser.rs
@@ -35,95 +35,38 @@ impl From<&str> for Line {
 mod tests {
     use super::Line;
 
-    macro_rules! test_group {
-        ($name:ident, $($test:expr),*) => (
-            #[test]
-            fn $name() {
-                for test in [
-                    $(
-                        $test,
-                    )*
-                    ].iter() {
-                    test.assert();
-                }
-            }
-        )
+    macro_rules! testfrom {
+        ($input:expr, $($a:ident => $b:expr),*) => {
+            assert_eq!(Line::from($input),
+            Line {
+                $( $a: Some($b.into()), )* ..Default::default()
+            });
+        };
     }
 
-    struct TestCase {
-        input: &'static str,
-        output: Line,
-        failer: bool,
+    #[test]
+    fn test_creation_from_strings() {
+        testfrom!(": ", label=>"");
+        testfrom!(":  instr", label=>"", instruction=>"instr");
+        testfrom!("label:", label=>"label");
+        testfrom!("op", instruction=>"op");
+        testfrom!(";comm", comment=>"comm");
+        testfrom!("next: lda ($24),x ; load from the table",
+            label=>"next", 
+            instruction=>"lda ($24),x", 
+            comment=>" load from the table");
+        //testfrom!("beq :+", instruction=>"beq :+");
     }
 
-    impl TestCase {
-        fn new(input: &'static str) -> Self {
-            TestCase {
-                input: input,
-                output: Line::default(),
-                failer: false,
-            }
-        }
-        fn label(mut self, value: &'static str) -> Self {
-            self.output.label = Some(String::from(value));
-            self
-        }
-        fn instruction(mut self, value: &'static str) -> Self {
-            self.output.instruction = Some(String::from(value));
-            self
-        }
-        fn comment(mut self, value: &'static str) -> Self {
-            self.output.comment = Some(String::from(value));
-            self
-        }
-        fn should_fail(mut self) -> Self {
-            self.failer = true;
-            self
-        }
-        fn assert(&self) {
-            let found = Line::from(self.input);
-            match self.failer {
-                false => assert!(
-                    found == self.output,
-                    "Test assertion failed\n     Input: '{}'\n  Expected: {:?}\n     Found: {:?}\n",
-                    self.input,
-                    self.output,
-                    found
-                ),
-                true => assert!(
-                    found != self.output,
-                    "Test assertion should fail but it did not\n         Input: '{}'\n  Not expected: {:?}\n         Found: {:?}\n",
-                    self.input,
-                    self.output,
-                    found
-                )
-            }
-        }
+    #[test]
+    fn test_empty_space_removal() {
+        testfrom!("  a:", label=>"a");
+        testfrom!("  a  :", label=>"a");
+        testfrom!("a  :", label=>"a");
+        testfrom!("a:", label=>"a");
+        testfrom!(":", label=>"");
+        testfrom!(";  co", comment=>"  co");
+        testfrom!(";co", comment=>"co");
+        //testfrom!(";co  ", comment=>"co");
     }
-
-    test_group!(
-        test_creation_from_strings,
-        TestCase::new("next: lda ($24),x ; load from the table")
-            .label("next")
-            .instruction("lda ($24),x")
-            .comment(" load from the table"),
-        TestCase::new(":   instr").label("").instruction("instr"),
-        TestCase::new("label:").label("label"),
-        TestCase::new("operation").instruction("operation"),
-        TestCase::new(";comment").comment("comment"),
-        TestCase::new("beq :+").instruction("beq :+").should_fail()
-    );
-
-    test_group!(
-        test_empty_space_removal,
-        TestCase::new("  a:").label("a"),
-        TestCase::new("  a  :").label("a"),
-        TestCase::new("a  :").label("a"),
-        TestCase::new("a:").label("a"),
-        TestCase::new(":").label(""),
-        TestCase::new(":   ").label(""),
-        TestCase::new(";   co").comment("   co"),
-        TestCase::new(";co").comment("co"),
-        TestCase::new(";co  ").comment("co").should_fail()
-    );
 }
