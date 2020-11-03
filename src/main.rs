fdd7c77255dcd3429d5b15a92349eb0cd23998a2
Guillermo Indalecio Fernandez <guillermobox@gmail.com>
2020-11-04 00:33:41 +0100

    Add all instructions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
 mod instructions;
 mod parser;
 
-fn main() {}
+use instructions::Instruction;
+
+fn main() {
+    match std::env::args().collect::<Vec<String>>()[1]
+        .parse()
+        .unwrap()
+    {
+        Instruction::LDA => println!("Load accumulator"),
+        Instruction::STA => println!("Store accumulator"),
+        _ => println!("Something else"),
+    }
+}
