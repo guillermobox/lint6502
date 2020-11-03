fe8476cf0eb7e4c8402bb4e71e90b834c1e58f5c
Guillermo Indalecio Fernandez <guillermobox@gmail.com>
2020-11-04 00:07:07 +0100

    Add another instruction

--- src/instructions.rs
+++ src/instructions.rs
@@ -4,16 +4,20 @@ use std::str::FromStr;
 #[derive(Debug, PartialEq)]
 enum Instruction {
     LDA,
+    STA,
 }
 
 impl FromStr for Instruction {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let r = Regex::new(r"(?i)(lda)").unwrap();
-        let a = r.captures(s);
-        match a {
-            Some(_) => Ok(Self::LDA),
+        let r = Regex::new(r"(?i)^(lda|sta)$").unwrap();
+        match r.captures(s) {
+            Some(a) => match a.get(1).unwrap().as_str().to_lowercase().as_str() {
+                "lda" => Ok(Self::LDA),
+                "sta" => Ok(Self::STA),
+                _ => Err(()),
+            },
             None => Err(()),
         }
     }
@@ -28,6 +32,8 @@ mod tests {
         assert_eq!(Ok(Instruction::LDA), "lda".parse());
         assert_eq!(Ok(Instruction::LDA), "LDA".parse());
         assert_eq!(Ok(Instruction::LDA), "Lda".parse());
-        assert_eq!("sta".parse::<Instruction>().is_err(), true);
+        assert_eq!(Ok(Instruction::STA), "sta".parse());
+        assert_eq!(Ok(Instruction::STA), "STA".parse());
+        assert_eq!(Ok(Instruction::STA), "Sta".parse());
     }
 }
