34fe6c8d9a4c44290133a497af61a01407dc27f2
Guillermo Indalecio Fernandez <guillermobox@gmail.com>
2020-11-04 00:15:28 +0100

    Cleanup main

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,4 @@
 mod instructions;
 mod parser;
-use std::io::BufRead;
 
-fn iterator(filename: &str) -> impl Iterator {
-    let fhandler = std::fs::File::open(filename).unwrap();
-    let reader = std::io::BufReader::new(fhandler);
-    reader.lines().map(|l| l.unwrap())
-}
-
-fn main() {
-    let filepath = std::env::args().nth(1).expect("Give me an argument");
-}
+fn main() {}
