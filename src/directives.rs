88bfa1742487055e8fdca47782347811c3d62079
Guillermo Indalecio Fernandez <guillermobox@gmail.com>
2020-11-06 22:28:05 +0100

    Trim white space at the beginning of a directive

--- src/directives.rs
+++ src/directives.rs
@@ -11,7 +11,7 @@ impl FromStr for Directive {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match Regex::new(r"^\.([^\s]+)\s+(.+)$").unwrap().captures(s) {
+        match Regex::new(r"^\s*\.([^\s]+)\s+(.+)$").unwrap().captures(s) {
             Some(capture) => Ok(Directive {
                 operation: capture.get(1).unwrap().as_str().into(),
                 operand: capture.get(2).unwrap().as_str().into(),
